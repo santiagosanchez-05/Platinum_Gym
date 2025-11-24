f6f520d05c8c406440ba303fa1a86006c50f713a
santiagosanchez-05 <saa0035135@est.univalle.edu>
2025-11-24 10:39:28 -0400

    initial commit

---
+++ Platinum_Gym_System/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Platinum_Gym_System.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId {  get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")] //Es obligatorio
+        [Display(Name = "Razon social")] // Cambiar el nombre en pantalla
+        [StringLength(50, ErrorMessage = "{0} must be: minimum {2} and maxmum {1} character long ", MinimumLength = 3)]
+        public string? BillingName { get; set; }
+        [Required(ErrorMessage = "El CI es obligatorio.")]
+        [RegularExpression(@"^\d{5,10}(-?[A-Za-z]{1,2})?$",
+         ErrorMessage = "Formato inválido. Ejemplos válidos: 1234567, 1234567LP, 1234567-LP, 1234567A")]
+        public string CI { get; set; }
+
+
+    }
+}
