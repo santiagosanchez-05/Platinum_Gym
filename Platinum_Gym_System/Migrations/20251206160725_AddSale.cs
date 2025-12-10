030ea5316f8bdfbd3abd0bf8099f41b2c2f0acf1
santiagosanchez-05 <saa0035135@est.univalle.edu>
2025-12-10 01:45:33 -0400

    correccion listado de  productos

--- Platinum_Gym_System/Migrations/20251206160725_AddSale.cs
+++ Platinum_Gym_System/Migrations/20251206160725_AddSale.cs
@@ -11,47 +11,47 @@ namespace Platinum_Gym_System.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.CreateTable(
-            //    name: "Sales",
-            //    columns: table => new
-            //    {
-            //        SaleId = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        SaleDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-            //        Total = table.Column<double>(type: "float", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Sales", x => x.SaleId);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    SaleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SaleDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.SaleId);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "SaleDetails",
-            //    columns: table => new
-            //    {
-            //        DetailId = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        SaleId = table.Column<int>(type: "int", nullable: false),
-            //        ProductId = table.Column<int>(type: "int", nullable: false),
-            //        Quantity = table.Column<int>(type: "int", nullable: false),
-            //        Subtotal = table.Column<double>(type: "float", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_SaleDetails", x => x.DetailId);
-            //        table.ForeignKey(
-            //            name: "FK_SaleDetails_Products_ProductId",
-            //            column: x => x.ProductId,
-            //            principalTable: "Products",
-            //            principalColumn: "ProductId",
-            //            onDelete: ReferentialAction.Cascade);
-            //        table.ForeignKey(
-            //            name: "FK_SaleDetails_Sales_SaleId",
-            //            column: x => x.SaleId,
-            //            principalTable: "Sales",
-            //            principalColumn: "SaleId",
-            //            onDelete: ReferentialAction.Cascade);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "SaleDetails",
+                columns: table => new
+                {
+                    DetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SaleId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Subtotal = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleDetails", x => x.DetailId);
+                    table.ForeignKey(
+                        name: "FK_SaleDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SaleDetails_Sales_SaleId",
+                        column: x => x.SaleId,
+                        principalTable: "Sales",
+                        principalColumn: "SaleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
             migrationBuilder.CreateTable(
                 name: "SalePayments",
@@ -77,15 +77,15 @@ namespace Platinum_Gym_System.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_SaleDetails_ProductId",
-            //    table: "SaleDetails",
-            //    column: "ProductId");
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleDetails_ProductId",
+                table: "SaleDetails",
+                column: "ProductId");
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_SaleDetails_SaleId",
-            //    table: "SaleDetails",
-            //    column: "SaleId");
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleDetails_SaleId",
+                table: "SaleDetails",
+                column: "SaleId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_SalePayments_SaleId",
--- Platinum_Gym_System/obj/Debug/net8.0/Platinum_Gym_System.AssemblyInfo.cs
+++ Platinum_Gym_System/obj/Debug/net8.0/Platinum_Gym_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platinum_Gym_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9dd3a0f5acccf6fd9931b3cc17a0bd0d48746f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42a6001feee1ef6439b5679189ea4e2ff2ae42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Platinum_Gym_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Platinum_Gym_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
